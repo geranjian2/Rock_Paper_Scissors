1b7836c7de0cac31cd5a4957cb561ea850511f5d
geranjian <geranjian@gmail.com>
2021-06-24 08:00:38 -0500

    Ajuste file

--- js/game/scissors.js
+++
@@ -1,7 +0,0 @@
-import { objects } from './enums/enums.js';
-class Scdcd {
-    type = objects.SCISSORS;
-    image = './images/icon-scissors.svg';
-    classHtml = "border-scissors circle-shadow-scissors";
-}
-export const scis = new Scdcd();
